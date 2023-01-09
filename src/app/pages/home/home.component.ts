dff93752efbe05436cf3612ea6cbd8e6b4729389
ElvisStwar <elvis.stwar@hotmail.com>
2023-01-09 14:50:20 -0500

    Json Pipes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,8 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
   nombre!:string;
   valor!:number;
   moneda = "";
   otro!:string
+  datos!:object
+
+  imprimir(){
+    this.datos =
+    [
+      {
+        "Apellido": "Arcila",
+        "Casado": false,
+        "Dirección": "Calle 35 # 43 28",
+        "Nombre": "Diego",
+        "Teléfono": 3859720
+      },
+      {
+        "Apellido": "Bueno",
+        "Casado": false,
+        "Dirección": "CR 16A # 53 28",
+        "Nombre": "Kevin",
+        "Teléfono": 31485579954
+      },
+      {
+        "Apellido": "Palomino",
+        "Casado": false,
+        "Dirección": "CR 25 # 52 33",
+        "Nombre": "Natalia",
+        "Teléfono": 32255945555
+      }
+    ]
+  }
+
 }
